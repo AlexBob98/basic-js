227c50c925c3e516102c21e6653c4655bb40d2a9
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-05-02 16:54:42 +0300

    feat: add code for recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+// помог на 90% google 
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(arr) {
+    let counter = 1;
+    let max = 0
+    for (let a of arr) {
+      if (Array.isArray(a)) {
+        max = Math.max(this.calculateDepth(a), max);
+      }
+    }
+    return counter + max;
+}
 }
 
 module.exports = {
